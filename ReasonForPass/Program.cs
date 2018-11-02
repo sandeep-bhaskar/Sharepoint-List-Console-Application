3634da0a9c9038fdd5cf90b69c30f9b4630ee99c
sandeep <sandeep.d@technovert.com>
2018-11-02 16:45:45 +0530

    Add project files.

---
+++ ReasonForPass/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security;
+using Microsoft.SharePoint.Client;
+
+namespace ReasonForPass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SharePoint Online site URL:");
+            string webSPOUrl = Console.ReadLine();
+            Console.WriteLine("User Name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Password:");
+            SecureString password = FetchPasswordFromConsole();
+            Services services = new Services(webSPOUrl,userName,password); 
+
+            Console.ForegroundColor = ConsoleColor.White;
+            var items = services.GetListData("SanTest Company");
+            foreach (ListItem list in items)
+            {
+                Console.WriteLine("List title is: " + list["Title"].ToString());
+            }
+            Console.WriteLine("");
+        }
+
+        private static SecureString FetchPasswordFromConsole()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        password = password.Substring(0, password.Length - 1);
+                        var pos = Console.CursorLeft;
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            var securePassword = new SecureString();
+            //Convert string to secure string  
+            foreach (char c in password)
+                securePassword.AppendChar(c);
+            securePassword.MakeReadOnly();
+            return securePassword;
+        }
+    }
+}
---
+++ ReasonForPass/Services.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReasonForPass
+{
+   public class Services
+    {
+       public static ClientContext Context { get; set; }
+
+        public Services(string webSPOUrl,string userName,SecureString password) {
+
+            // Starting with ClientContext, the constructor requires a URL to the 
+            // server running SharePoint. 
+            Context = new ClientContext(webSPOUrl);
+            Context.Credentials = new SharePointOnlineCredentials(userName, password);
+        }
+
+        public ListItemCollection GetListData(string listName) {
+            // Assume the web has a list named "Announcements". 
+            List list = Context.Web.Lists.GetByTitle(listName);
+
+            // This creates a CamlQuery that has a RowLimit of 100, and also specifies Scope="RecursiveAll" 
+            // so that it grabs all list items, regardless of the folder they are in. 
+            CamlQuery query = CamlQuery.CreateAllItemsQuery(100);
+            ListItemCollection items = list.GetItems(query);
+            // Assume there is a list item with ID=1. 
+            ListItem listItem = list.GetItemById(1);
+
+            // Write a new value to the Body field of the Announcement item.
+            listItem["Title"] = "This is my new value!!";
+            listItem.Update();
+
+            // Retrieve all items in the ListItemCollection from List.GetItems(Query). 
+            Context.Load(items);
+            Context.ExecuteQuery();
+            return items;
+        }
+    }
+}
